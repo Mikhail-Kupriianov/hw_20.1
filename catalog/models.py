dc25968e1f299749457331ee95a88ecd1bf86b77
Mkupr <mkupr@outlook.com>
2023-06-21 00:16:40 +0300

    finish hw_21_1

--- catalog/models.py
+++ catalog/models.py
@@ -1,21 +1,20 @@
+import datetime
 from django.db import models
-
 
 NULLABLE = {'null': True, 'blank': True}
 
 
 class Product(models.Model):
-
     product_name = models.CharField(max_length=100, verbose_name='наименование')
     product_description = models.TextField(verbose_name='описание')
     product_preview = models.ImageField(upload_to='product/', verbose_name='изображение', **NULLABLE)
     product_category = models.CharField(max_length=100, verbose_name='категория')
     product_cost = models.FloatField(verbose_name='цена')
-    product_data_created = models.DateField(verbose_name='создан')
-    product_last_data_change = models.DateField(verbose_name='последнее изменение')
+    product_data_created = models.DateField(verbose_name='создан', default=datetime.date.today)
+    product_last_data_change = models.DateField(verbose_name='последнее изменение', default=datetime.date.today)
 
     def __str__(self):
-        return f"{self.product_name} {self.product_cost}"
+        return f"{self.product_name}\nЦена: {self.product_cost}"
 
     class Meta:
         verbose_name = 'продукт'
@@ -24,10 +23,8 @@ class Product(models.Model):
 
 
 class Category(models.Model):
-
     category_name = models.CharField(max_length=100, verbose_name='наименование')
     category_description = models.TextField(verbose_name='описание')
-
 
     def __str__(self):
         return f"{self.category_name} {self.category_description}"
@@ -36,3 +33,4 @@ class Category(models.Model):
         verbose_name = 'категория'
         verbose_name_plural = 'категории'
         ordering = ('category_name',)
+
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,12 +1,16 @@
 from django.urls import path
 
-from catalog.views import index, contact
+from blog.views import BlogListView
+from catalog.views import ProductListView, contact  # MessageSend
+
 
 from catalog.apps import CatalogConfig
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', index, name='index'),
-    path('contacts/', contact, name='contacts')
+    path('', ProductListView.as_view(), name='index'),
+    path('contacts/', contact, name='contacts'),
+    path('blogs/', BlogListView.as_view(), name='blogs'),
+    # path('contacts/', MessageSend.as_view(), name='contacts'),
 ]
