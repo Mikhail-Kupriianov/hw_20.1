ab00143028d975bd1fde5ea4e4929473d2c968e8
Mkupr <mkupr@outlook.com>
2023-06-30 21:40:10 +0300

    add py files for users app Task_1 hw_22_2

--- users/models.py
+++ users/models.py
@@ -5,5 +5,13 @@ from catalog.models import NULLABLE
 
 
 class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True, verbose_name='почта')
+
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
+    country = models.CharField(max_length=60, verbose_name='страна')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
